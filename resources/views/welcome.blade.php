208f124541cf7a0b84bd51923c428653032b0991
Rafaela Leal <rafaelalealmartinss@gmail.com>
2021-09-01 19:24:04 -0300

    [WIP] update name sections

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -56,8 +56,8 @@ https://templatemo.com/tm-535-softy-pinko
                         <!-- ***** Menu Start ***** -->
                         <ul class="nav">
                             <li><a href="#welcome" class="active">Home</a></li>
-                            <li><a href="#features">About</a></li>
-                            <li><a href="#work-process">Work Process</a></li>
+                            <li><a href="#features">About Us</a></li>
+                            <li><a href="#work-process">Window Cleaning</a></li>
                             <li><a href="#testimonials">Testimonials</a></li>
                             <li><a href="#contact-us">Contact Us</a></li>
                         </ul>
@@ -104,7 +104,7 @@ https://templatemo.com/tm-535-softy-pinko
                                 <div class="icon">
                                     <i><img src="images/featured-item-01.png" alt=""></i>
                                 </div>
-                                <h5 class="features-title">Modern Strategy</h5>
+                                <h5 class="features-title">Professional Organizing</h5>
                                 <p>Customize anything in this template to fit your website needs</p>
                             </div>
                         </div>
@@ -116,7 +116,7 @@ https://templatemo.com/tm-535-softy-pinko
                                 <div class="icon">
                                     <i><img src="images/featured-item-01.png" alt=""></i>
                                 </div>
-                                <h5 class="features-title">Best Relationship</h5>
+                                <h5 class="features-title">Packing and Unpacking</h5>
                                 <p>Contact us immediately if you have a question in mind</p>
                             </div>
                         </div>
@@ -128,7 +128,7 @@ https://templatemo.com/tm-535-softy-pinko
                                 <div class="icon">
                                     <i><img src="images/featured-item-01.png" alt=""></i>
                                 </div>
-                                <h5 class="features-title">Ultimate Marketing</h5>
+                                <h5 class="features-title">Residential Cleaning</h5>
                                 <p>You just need to tell your friends about our free templates</p>
                             </div>
                         </div>
@@ -194,7 +194,7 @@ https://templatemo.com/tm-535-softy-pinko
                 <div class="row">
                     <div class="offset-lg-3 col-lg-6">
                         <div class="info">
-                            <h1>Work Process</h1>
+                            <h1>Window Cleaning</h1>
                             <p>Aenean nec tempor metus. Maecenas ligula dolor, commodo in imperdiet interdum, vehicula ut ex. Donec ante diam.</p>
                         </div>
                     </div>
