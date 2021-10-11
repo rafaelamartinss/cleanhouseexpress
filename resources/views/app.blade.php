5c5aee8e2fb34114eb651da989d306db25d507a8
Rafaela Leal <rafaelalealmartinss@gmail.com>
2021-10-11 11:05:45 -0300

    [UI] update footer layout

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -69,19 +69,8 @@
     <footer>
         <div class="container">
             <div class="row">
-                <div class="col-lg-12 col-md-12 col-sm-12">
-                    <ul class="social">
-                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                        <li><a href="#"><i class="fa fa-rss"></i></a></li>
-                        <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="row">
                 <div class="col-lg-12">
-                    <p class="copyright">Copyright &copy; 2020 Softy Pinko Company - Design: TemplateMo</p>
+                    <p class="copyright">Copyright &copy; 2021 ThinkClean Company - Design: TemplateMo</p>
                 </div>
             </div>
         </div>
